1c0ed7f725e95a8e420540402db6b0a6b0ebed15
Deepanshu Dixit <deepanshu6255@gmail.com>
2024-09-06 17:57:33 +0530

    Added routes and provided access to the children component to access these routes

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router";
+import { ApplicationConfig } from "@angular/core";
+
+import { routes } from "./app.routes";
+
+export const appConfig: ApplicationConfig = {
+    providers: [provideRouter(routes, withComponentInputBinding(), withRouterConfig({
+        paramsInheritanceStrategy: 'always'
+    }))]
+}
