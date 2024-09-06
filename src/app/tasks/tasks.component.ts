2c55312221ad60b2f56ff2cf92defb56d01c3404
Deepanshu Dixit <deepanshu6255@gmail.com>
2024-09-06 19:55:19 +0530

    Added sorting for the tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,20 +1,32 @@
 import { Component, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   userId = input.required<string>();
+  order = input<'asc' | 'desc'>('asc');
 
   private taskService = inject(TasksService);
 
-  userTasks = computed(() => this.taskService.allTasks().filter((task) => task.userId === this.userId()))
+  userTasks = computed(
+    () =>
+      this.taskService
+        .allTasks()
+        .filter((task) => task.userId === this.userId())
+        .sort((a, b) => {
+          if (!this.order() || this.order() === 'desc') {
+            return a.id > b.id ? -1 : 1
+          }
+          return a.id > b.id ? 1 : -1
+        })
+  );
 }
